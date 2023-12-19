aca3b752e8d762792672b5ed322de29965ceb952
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2023-12-18 19:48:45 -0600

    initial commit

---
+++ spec/command.spec.js
@@ -0,0 +1,19 @@
+import Command from '../command.js'
+
+describe("Command class", function() {
+
+  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  });
+
+  it("constructor sets command type", function() {
+    let testCommandClass = new Command("a", "b");
+      expect(testCommandClass.commandType).toBe("a");
+    });
+
+    it("constructor sets value", function() {
+      let testCommandClass = new Command("a", "b");
+        expect(testCommandClass.value).toBe("b");
+      });
+
+});
---
+++ spec/rover.spec.js
@@ -0,0 +1,78 @@
+import Rover from '../rover.js'
+import Message from '../message.js'
+import Command from '../command.js'
+
+
+describe("Rover class", function() {
+
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let testRoverClass = new Rover("a");
+    expect(testRoverClass.position).toBe("a");
+    expect(testRoverClass.mode).toBe("NORMAL");
+    expect(testRoverClass.generatorWatts).toBe(110);
+  })
+
+  it("response returned by receiveMessage contains the name of the message", function() {
+    let testCommand = [new Command('STATUS_CHECK')];
+    let testMessage = new Message("Check the rover status", testCommand);
+    let testRover = new Rover(98382);
+    let checkReceiveMessage = testRover.receiveMessage(testMessage);
+    expect(checkReceiveMessage.message).toBe("Check the rover status");
+  })
+
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function () {
+    let testCommand = [new Command('MODE_CHANGE', "LOW_POWER"), new Command('STATUS_CHECK')];
+    let testMessage = new Message("Change mode to low power, check status", testCommand);
+    let testRover = new Rover(98382);
+    let checkReceiveMessage = testRover.receiveMessage(testMessage);
+    expect(checkReceiveMessage["results"].length).toBe(testCommand.length);
+    
+  })
+
+  it("responds correctly to the status check command", function () {
+    let testCommand = [new Command('STATUS_CHECK')];
+    let testMessage = new Message("Check the rover status", testCommand);
+    let testRover = new Rover(98382);
+    let checkReceiveMessage = testRover.receiveMessage(testMessage);
+
+    expect(typeof checkReceiveMessage.results[0].roverStatus).toBe("object");
+    expect(checkReceiveMessage.results[0].roverStatus.position).toBe(testRover.position);
+    expect(checkReceiveMessage.results[0].roverStatus.mode).toBe(testRover.mode);
+    expect(checkReceiveMessage.results[0].roverStatus.generatorWatts).toBe(testRover.generatorWatts);
+
+  })
+
+  it("responds correctly to the mode change command", function () {
+    let testCommand = [new Command('MODE_CHANGE', "LOW_POWER")];
+    let testMessage = new Message("Change mode to low power.", testCommand);
+    let testRover = new Rover(98382);
+
+    testRover.receiveMessage(testMessage);
+
+    expect(testRover.mode).toBe(testCommand[0].value);
+
+  })
+
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function () {
+    let testCommand = [new Command('MODE_CHANGE', "LOW_POWER"), new Command('MOVE', 3), new Command("STATUS_CHECK")];
+    let testMessage = new Message("Change mode to low power, move rover, status check.", testCommand);
+    let testRover = new Rover(98382);
+    let checkReceiveMessage = testRover.receiveMessage(testMessage)
+
+    expect(checkReceiveMessage.results[1].completed).toBe(false);
+    expect(checkReceiveMessage.results[2].roverStatus.position).toBe(98382);
+
+  })
+
+  it("responds with the position for the move command", function () {
+    let testCommand = [new Command('MOVE', 3), new Command("STATUS_CHECK")];
+    let testMessage = new Message("Move rover.", testCommand);
+    let testRover = new Rover(98382);
+    let checkReceiveMessage = testRover.receiveMessage(testMessage);
+
+    expect(checkReceiveMessage.results[0].completed).toBe(true);
+    expect(checkReceiveMessage.results[1].roverStatus.position).toBe(testCommand[0].value);
+
+  })
+
+});
